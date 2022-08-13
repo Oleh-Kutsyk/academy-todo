8cba5d7804a5fd209b89b3825ccdf8389c8cbf4f
Oleh Kutsyk <oleh.kutsyk@israelit.pro>
2022-08-13 20:24:31 +0300

    add todo component

---
+++ src/components/todo/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const Todo = ({todo, type, ...action}) => {
+    return (
+        <div className='todo' key={todo.id}>
+            <div className='mainInfo'>
+                {type !== 'deleted' &&
+                    <input
+                        className='checkbox'
+                        checked={todo.isCompleted}
+                        type="checkbox"
+                        onChange={() => action.toggleCompleted(todo.id)}/>
+                }
+                <p className={todo.isCompleted ? 'completed title' : 'title'}>{todo.title}</p>
+            </div>
+            {type !== 'deleted' ?
+                <div className='actions'>
+                    <div>
+                        <label htmlFor="favorite">Favorite</label>
+                        <input
+                            className='checkbox'
+                            id='favorite'
+                            checked={todo.isFavorite}
+                            type="checkbox"
+                            onChange={() => action.toggleFavorite(todo.id)}
+                        />
+                    </div>
+                    <button className='delete' onClick={() => action.deleteTodo(todo.id)}>delete todo</button>
+                </div> :
+                <button className='delete' onClick={() => action.deleteTodo(todo.id)}>Return todo</button>
+            }
+        </div>
+    )
+}
--- src/components/todos/index.js
+++ src/components/todos/index.js
@@ -1,38 +1,12 @@
 import React from 'react';
+import {Todo} from "../todo";
 
 export const Todos = ({todos, type, ...action}) => {
 
     return (
         <React.Fragment>
             {todos.map(todo => (
-                <div className='todo' key={todo.id}>
-                    <div className='mainInfo'>
-                        {type !== 'deleted' &&
-                            <input
-                                className='checkbox'
-                                checked={todo.isCompleted}
-                                type="checkbox"
-                                onChange={() => action.toggleCompleted(todo.id)}/>
-                        }
-                        <p className={todo.isCompleted ? 'completed title' : 'title'}>{todo.title}</p>
-                    </div>
-                    {type !== 'deleted' ?
-                        <div className='actions'>
-                            <div>
-                                <label htmlFor="favorite">Favorite</label>
-                                <input
-                                    className='checkbox'
-                                    id='favorite'
-                                    checked={todo.isFavorite}
-                                    type="checkbox"
-                                    onChange={() => action.toggleFavorite(todo.id)}
-                                />
-                            </div>
-                            <button className='delete' onClick={() => action.deleteTodo(todo.id)}>delete todo</button>
-                        </div> :
-                        <button className='delete' onClick={() => action.deleteTodo(todo.id)}>Return todo</button>
-                    }
-                </div>
+                <Todo key={todo.id} todo={todo} type={type} {...action}/>
             ))}
         </React.Fragment>
     );
